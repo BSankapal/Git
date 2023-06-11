d1139f0a5984d49d4c7b169a2b43fea9399e90b1
BSankapal <balajibsankapal@gmail.com>
2023-06-11 12:03:18 +0530

    initial commit

---
+++ HW.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ll n, x;
+    cin>>n;
+    for(ll i=0;i<n;i++)
+    {
+        cin>>x;
+    }
+    return 0;
+}
