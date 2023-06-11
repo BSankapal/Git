a9bc68417dddf89c1367710190dba70148e6fc09
BSankapal <balajibsankapal@gmail.com>
2023-06-11 12:11:08 +0530

    Final commit

--- HW.cpp
+++ HW.cpp
@@ -5,9 +5,9 @@ typedef long long int ll;
 int main(){
     ll n, x;
     cin>>n;
-    for(ll i=0;i<n;i++)
-    {
-        cin>>x;
-    }
+    // for(ll i=0;i<n;i++)
+    // {
+    //     cin>>x;
+    // }
     return 0;
 }
